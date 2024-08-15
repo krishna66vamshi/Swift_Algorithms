c985f188e8f48791b85622d72ca632e447462cc7
vamsi <34568037+krishna66vamshi@users.noreply.github.com>
2024-08-15 08:55:24 +0530

    two sum algorithms added

--- Swift_Algorithms/Swift_Algorithms/ViewController.swift
+++ Swift_Algorithms/Swift_Algorithms/ViewController.swift
@@ -12,10 +12,68 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        print(printFibonacciSeries(upto: 8)) // [0, 1, 1, 2, 3, 5, 8, 13]
+        
+        
+//        print(fibonacciSeries(upto: 8)) // [0, 1, 1, 2, 3, 5, 8, 13]
+        
+//        print(findArmstrongNumber(num: 153))
+        
+//        print(twoSumProblem(num: [3,4,5,6,8,1,2],target: 7))
+        
+//        print(twoSumProblemReturnIndexes(num: [3,4,5,6,8,10,20],target: 7)) //[1, 0]
+        
+
+        print(findRepeatedCharacters(str: "abcda")) //["a": 2, "c": 1, "d": 1, "b": 1]
+    }
+    
+    func findRepeatedCharacters(str:String) -> [Character:Int]{
+        var repeatedCharDict = [Character:Int]()
+        for char in str{
+            let dictValue = repeatedCharDict[char] ?? 0
+            repeatedCharDict.updateValue(dictValue + 1, forKey: char)
+        }
+        return repeatedCharDict
+    }
+    
+    func twoSumProblemReturnIndexes(num:[Int],target:Int) -> [Int]{
+        var dict = [Int:Int]()
+        var result = [Int]()
+
+        for (index,element) in num.enumerated(){
+            let compliment = target - index
+            print("compliment",compliment)
+            if let value = dict[compliment]{
+                print("value",value)
+                result.append(value)
+            }
+            
+            dict[element] = index
+        }
+        
+        print("dict",dict) // [6: 3, 8: 4, 20: 6, 5: 2, 4: 1, 3: 0, 10: 5]
+        
+        return result
+        
+    }
+    
+    
+    func twoSumProblem(num:[Int],target:Int) -> [(Int,Int)]{
+        var uniquePairs = Set<Int>()
+        var result = [(Int,Int)]()
+        
+        for i in num{
+            let compliment = target - i
+            if uniquePairs.contains(compliment){
+                result.append((compliment,i))
+            }
+            uniquePairs.insert(i)
+        }
+        
+        return result
+        
     }
 
-    func printFibonacciSeries(upto num:Int) -> [Int]{
+    func fibonacciSeries(upto num:Int) -> [Int]{
         
         var fibSeries = [0,1]
         
@@ -25,10 +83,28 @@ class ViewController: UIViewController {
         }
         
         return fibSeries
-        
-        
-        
     }
 
+    func findArmstrongNumber(num:Int) -> Bool{
+        
+        var givenNum = num
+        var result = 0
+       
+        while givenNum > 0 {
+            let lastDigit = givenNum % 10
+            result = result + (lastDigit * lastDigit * lastDigit)
+            givenNum = givenNum/10
+        }
+        
+        if num == result{
+            return true
+        }else{
+            return false
+        }
+    
+    }
+    
+    
+    
 }
 
